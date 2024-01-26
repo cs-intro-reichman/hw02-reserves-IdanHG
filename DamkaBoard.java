5517ba4192c24255bedb3c4ac4903f4b914da00c
Idan Harel Gross <idannhg@Idans-MacBook-Pro.local>
2024-01-26 15:01:37 +0200

    second try

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -8,10 +8,10 @@ public class DamkaBoard {
 		for (int i = 1 ; i <= n ; i++) {
 			for (int j = 1 ; j <= n ; j++) {
 				if (i%2 == 1 ) {
-					System.out.print("*" + "  ");
+					System.out.print("*" + " ");
 				}
 				else {
-					System.out.print("  " + "*");
+					System.out.print(" " + "*");
 				} 
 			}
 			System.out.println();
